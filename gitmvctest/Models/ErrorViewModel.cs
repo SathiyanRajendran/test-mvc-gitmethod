5f2d70dfb0fe2142ffaed6d6210d06d35c87f9a9
Sathiyan R <sathiyan@whiteblue.in>
2023-01-11 11:38:45 +0530

    first commit by sathiyan

---
+++ gitmvctest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace gitmvctest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
